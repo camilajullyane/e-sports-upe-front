b9a6c42e637b83c51be695d7232fc344efdd3509
camilajullyane <camilaj.silva0@gmail.com>
2025-06-19 20:16:57 -0300

    chore: adicionando shadcn ui e configurando arquivos

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,22 @@
 import { Routes, Route, Navigate } from "react-router";
 import { PublicRoute } from "./PublicRoute";
 import { LoginPage } from "../pages/Auth/LoginPage";
+import { SignUpPage } from "../pages/Auth/SignUpPage";
+import { ProtectedRoute } from "./ProtectedRoute";
+import { HomePage } from "../pages/Home";
 
 export function Router() {
   const authRoutes = (
     <Route element={<PublicRoute />}>
       <Route index element={<Navigate to="login" />} />
       <Route path="login" element={<LoginPage />} />
+      <Route path="signup" element={<SignUpPage />} />
+    </Route>
+  );
+
+  const protectedRoutes = (
+    <Route element={<ProtectedRoute />}>
+      <Route path="home" element={<HomePage />} />
     </Route>
   );
 
