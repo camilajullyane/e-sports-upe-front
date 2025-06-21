308daa9fe009116c8ac04b14e4d930742079e435
camilajullyane <camilaj.silva0@gmail.com>
2025-06-21 09:23:52 -0300

    feat: adicionando autenticação nas rotas protegidas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,8 +4,14 @@ import { LoginPage } from "../pages/Auth/LoginPage";
 import { SignUpPage } from "../pages/Auth/SignUpPage";
 import { ProtectedRoute } from "./ProtectedRoute";
 import { HomePage } from "../pages/Home";
+import { authStore } from "@/store/auth.store";
+import { Championship } from "@/pages/Championship";
+import { Team } from "@/pages/Team";
 
 export function Router() {
+  const { logged } = authStore().load();
+  const credetentials = authStore().getCredentials();
+
   const authRoutes = (
     <Route element={<PublicRoute />}>
       <Route index element={<Navigate to="signin" />} />
@@ -17,13 +23,20 @@ export function Router() {
   const protectedRoutes = (
     <Route element={<ProtectedRoute />}>
       <Route path="home" element={<HomePage />} />
+      <Route path="championship" element={<Championship />} />
+      <Route path="team" element={<Team />} />
     </Route>
   );
 
   return (
     <Routes>
       {authRoutes}
-      {protectedRoutes}
+
+      <Route
+        element={logged ? <ProtectedRoute /> : <Navigate to={"/signin"} />}
+      >
+        {protectedRoutes}
+      </Route>
     </Routes>
   );
 }
