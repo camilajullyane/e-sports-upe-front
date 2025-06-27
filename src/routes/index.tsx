8de549d58cbe60ebdabd684f032e9569252f856b
camilajullyane <camilaj.silva0@gmail.com>
2025-06-27 18:23:06 -0300

    feat: ajustando tamanho da sideBar

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -12,6 +12,12 @@ const InitialPageRouter = lazy(() =>
   }))
 );
 
+const HomePageRouter = lazy(() =>
+  import("@/pages/Home/Router").then((module) => ({
+    default: module.Router,
+  }))
+);
+
 const ChampionshipPageRouter = lazy(() =>
   import("@/pages/Championship/Router").then((module) => ({
     default: module.Router,
@@ -53,6 +59,7 @@ export function Router() {
     <Route element={<ProtectedRoute />}>
       <Route path="championship/*" element={<ChampionshipPageRouter />} />
       <Route path="team/*" element={<TeamPageRouter />} />
+      <Route path="home/*" element={<HomePageRouter />} />
     </Route>
   );
 
